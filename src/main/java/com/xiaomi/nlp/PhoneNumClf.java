b9e6e768faf891a0cd8d34cbdbf0c31d58535e07
daiyang [代阳] <daiyang@xiaomi.com>
2016-01-26 20:23:17 +0800

    phone-num-clf

--- src/main/java/com/xiaomi/nlp/PhoneNumClf.java
+++ src/main/java/com/xiaomi/nlp/PhoneNumClf.java
@@ -30,7 +30,6 @@ public class PhoneNumClf {
     int w1 = -1;
     int w2 = -1;
 
-    static String userCreditFile = "";
 
     static class UserCreditRow {
         float credit;
@@ -41,8 +40,56 @@ public class PhoneNumClf {
         }
     }
 
-    static HashMap<String, UserCreditRow> userCredit = new HashMap<String, UserCreditRow>();
-    static {
+    static class Vec {
+        static float[] fromString(String s) {
+            if (s.length() < 2) throw new IllegalArgumentException("Vec string length less than 2.");
+            s = s.substring(1, s.length() - 1);
+            String[] tmp = s.split(",");
+            float[] res = new float[tmp.length];
+            for (int i = 0; i < res.length; ++i) res[i] = Float.parseFloat(tmp[i]);
+            return res;
+        }
+
+        static float[] add(float[] A, float[] B) {
+            if (A.length != B.length) throw new IllegalArgumentException("Given Vecs are not the same Dimension.");
+            float[] res = new float[A.length];
+            for (int i = 0; i < A.length; ++i) res[i] = A[i] + B[i];
+            return res;
+        }
+
+        static float dot(float[] A, float[] B) {
+            if (A.length != B.length) throw new IllegalArgumentException("Given Vecs are not the same Dimension.");
+            float res = 0;
+            for (int i = 0; i < A.length; ++i) res += A[i] * B[i];
+            return res;
+        }
+
+        static float[] mul(float a, float[] B) {
+            float[] res = new float[B.length];
+            for (int i = 0; i < B.length; ++i) res[i] = a * B[i];
+            return res;
+        }
+
+        static float[] mul(float[] A, float[] B) {
+            float[] res = new float[A.length];
+            for (int i = 0; i < B.length; ++i) res[i] = A[i] * B[i];
+            return res;
+        }
+
+        static int argmax(float[] A) {
+            if (A.length == 0) return -1;
+            int res = 0;
+            for (int i = 0; i < A.length; ++i)
+                if (A[i] > A[res]) res = i;
+            return res;
+        }
+    }
+
+    private HashMap<String, UserCreditRow> userCredit = new HashMap<String, UserCreditRow>();
+    private HashMap<String, float[]> prior = new HashMap<String, float[]>();
+    private HashMap<Integer, String> feedbackDic = new HashMap<Integer, String>();
+
+    public PhoneNumClf(String userCreditFile, String priorFile, String feedbackDicFile) {
         try {
             BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(userCreditFile)));
             while (true) {
@@ -52,6 +99,22 @@ public class PhoneNumClf {
                 String imei = tmp[0];
                 userCredit.put(imei, new UserCreditRow(tmp));
             }
+
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(priorFile)));
+            while (true) {
+                String line = in.readLine();
+                if (line == null) break;
+                String[] tmp = line.split("\\t");
+                prior.put(tmp[0], Vec.fromString(tmp[1]));
+            }
+
+            in = new BufferedReader(new InputStreamReader(new FileInputStream(feedbackDicFile)));
+            while (true) {
+                String line = in.readLine();
+                if (line == null) break;
+                String[] tmp = line.split("\\t");
+                feedbackDic.put(Integer.parseInt(tmp[0]), tmp[1]);
+            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -59,44 +122,20 @@ public class PhoneNumClf {
         }
     }
 
-    private float getCredit(String no) {
-
+    private float getCredit(String imei) {
+        return 0;
     }
 
-    public float[] addVec(float[] A, float[] B) {
-        if (A.length != B.length) throw new IllegalArgumentException("Given Vecs are not the same Dimension.");
-        float[] res = new float[A.length];
-        for (int i = 0; i < A.length; ++i) res[i] = A[i] + B[i];
-        return res;
+    private float[] getFeedbackVotes(String imei, String text) {
+        return new float[0];
     }
 
-    public float dot(float[] A, float[] B) {
-        if (A.length != B.length) throw new IllegalArgumentException("Given Vecs are not the same Dimension.");
-        float res = 0;
-        for (int i = 0; i < A.length; ++i) res += A[i] * B[i];
-        return res;
-    }
-
-    public float[] mulVec(float a, float[] B) {
-        float[] res = new float[B.length];
-        for (int i = 0; i < B.length; ++i) res[i] = a * B[i];
-        return res;
-    }
-
-    public float[] mulVec(float[] A, float[] B) {
-        float[] res = new float[A.length];
-        for (int i = 0; i < B.length; ++i) res[i] = A[i] * B[i];
-        return res;
-    }
-
-    public int argmax(float[] A) {
-        if (A.length == 0) return -1;
-        int res = 0;
-        for (int i = 0; i < A.length; ++i)
-            if (A[i] > A[res]) res = i;
-        return res;
-    }
-
+    private float[] getPriorVotes(String no) {
+        for (String catRegex : prior.keySet()) {
+            if (no.matches(catRegex)) return prior.get(catRegex);
+        }
+        float[] res = new float[cat2name.length];
+k   }
     public int classify(String no, List<Mark> marks, List<Feedback> feedbacks, List<Record> records) {
         int numCat = cat2name.length;
 
@@ -112,19 +151,22 @@ public class PhoneNumClf {
         float[] feedbackVotes = new float[numCat];
         for (Feedback feedback: feedbacks) {
             float[] votes = getFeedbackVotes(feedback.imei, feedback.text);
-            feedbackVotes = addVec(feedbackVotes, votes);
+            feedbackVotes = Vec.add(feedbackVotes, votes);
         }
 
         if (w1 == -1) w1 = feedbacks.size();
         if (w2 == -1) w2 = marks.size();
 
-        float[] finalVotes = mulVec(addVec(mulVec(w2, userCreditVotes), mulVec(w1, feedbackVotes)), priorVotes);
+        float[] finalVotes = Vec.mul(Vec.add(Vec.mul(w2, userCreditVotes), Vec.mul(w1, feedbackVotes)), priorVotes);
 
-        int finalCat = argmax(finalVotes);
+        int finalCat = Vec.argmax(finalVotes);
         return finalCat;
     }
 
+
+
     public static void main(String[] args) {
+
     }
 }
 
